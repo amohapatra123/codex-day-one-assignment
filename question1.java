34ef0b654da0a4815e5f49ddd5165ddb9bf20f7b
amohapatra123 <amohapatra2001@gmail.com>
2019-08-24 17:50:12 +0530

    Update question1.java

--- question1.java
+++ question1.java
@@ -3,7 +3,7 @@ public class question1 {
 
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
+	
 		System.out.println(5/2);
 		System.out.println(5/2.0);
 		char ch='a';
