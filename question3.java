a95bde01a23dfb6cc1799b8de02e58a252ce55cd
amohapatra123 <amohapatra2001@gmail.com>
2019-08-24 17:51:36 +0530

    Update question3.java

--- question3.java
+++ question3.java
@@ -3,7 +3,7 @@ public class question3 {
 
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
+		
 		int a=7,b=9;
 		System.out.println(a&b);
 		System.out.println(a|b);
