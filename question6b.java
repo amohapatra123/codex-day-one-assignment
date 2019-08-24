945f0f48df14575e964aa563c4ee7dcf866e1d8d
amohapatra123 <amohapatra2001@gmail.com>
2019-08-24 17:47:54 +0530

    Create question6b.java

---
+++ question6b.java
@@ -0,0 +1,26 @@
+
+public class question6b {
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		int n=2019;
+		if(n%4==0)
+		{
+			if(n%100==00)
+			{
+				if(n%400==0)
+				{
+					System.out.println("yes a leap year");
+				}
+			}
+		}
+		else 
+		{
+			System.out.println("Not a leap year");
+			
+		}
+
+	}
+
+}
