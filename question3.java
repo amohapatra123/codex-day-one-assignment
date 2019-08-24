af13a249feedcaacefc3b9b6dc20a972d5f3e42f
amohapatra123 <amohapatra2001@gmail.com>
2019-08-24 17:45:36 +0530

    Create question3.java

---
+++ question3.java
@@ -0,0 +1,17 @@
+
+public class question3 {
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		int a=7,b=9;
+		System.out.println(a&b);
+		System.out.println(a|b);
+		System.out.println(a^b);
+		System.out.println(a<<2);
+		System.out.println(a>>2);
+		
+
+	}
+
+}
